0d387451d385b163a303b5fdebd825c94b6fe7bf
Artem007112 <158475211+Artem007112@users.noreply.github.com>
2024-02-08 15:58:23 +0300

    Add files via upload

---
+++ mdk/script.js
@@ -0,0 +1,27 @@
+
+document.write(`
+<header>
+<div style="float: left;">
+<a href=""><img src="img/gerb.png"></a>
+
+<p>352190, Россия, Краснодарский край, Гулькевичский район, г. Гулькевичи, ул. Малиновского, 36</p>
+<p style="font-size:14pt;">Городское поселение <b>Гулькевичи</b> МО Гулькевичский район</p>
+</div>
+</header>
+<nav>
+</nav>
+<main>
+<aside>
+<li>
+<a href='index.html'><ul>Главная</ul></a>
+<a href='storypage.html'><ul>История Города</ul></a>
+<a href='infro.html'><ul>Инфраструктура</ul></a>
+<a href='aboutme.html'><ul>О разработчике
+<p style="display:none;">Артём</p>
+<p style="display:none;">Олег</p>
+</ul></a>
+
+
+
+</li>
+`)
